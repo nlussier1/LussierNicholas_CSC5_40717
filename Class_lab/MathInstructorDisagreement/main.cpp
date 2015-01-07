3e9d644e2fb83f03db6a9e8d6d29dd3209590e73
nlussier1 <Nick_Lussier@Nicholass-MacBook-Pro.local>
2015-01-07 11:31:38 -0800

    Added Classwork

---
+++ Class_lab/MathInstructorDisagreement/main.cpp
@@ -0,0 +1,35 @@
+/* Process: Exploratory Example for GitHub
+ * File:   main.cpp
+ * Author: Nick_Lussier
+ *  Purpose: How easy computers can make mistakes
+ * Created on January 7, 2015, 11:17 AM
+ */
+//system libraries
+#include <iostream>
+
+//user libraries
+
+//global constant
+
+//prototype functions
+
+
+
+using namespace std;
+
+//execution begins here
+int main(int argc, char** argv) {
+    //Declare Variables
+    short a, b, c;
+    //prompt for a and b
+    cout<<"Input 2 short values"<<endl;
+    cin>>a;
+    cin>>b;
+    //sum the values
+    c=a+b;
+    //Output Values
+    cout<<c<<" = "<<a<<" + "<<b<<endl;
+    //Minotaur in house
+    return 0;
+}
+
