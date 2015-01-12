edc916471a487a79a7970f4ff627bc28b0dbcb7c
nlussier1 <Nick_Lussier@nicholass-mbp.attlocal.net>
2015-01-11 21:10:40 -0800

    Updated for Homework

---
+++ HMWK/Assignment 1/(E)Savitch9thEd_Chap1_PracProg5/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-MacOSX
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-MacOSX
+CND_ARTIFACT_NAME_Debug=_e_savitch9thed_chap1_pracprog5
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-MacOSX/_e_savitch9thed_chap1_pracprog5
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-MacOSX/package
+CND_PACKAGE_NAME_Debug=(e)savitch9thedchap1pracprog5.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-MacOSX/package/(e)savitch9thedchap1pracprog5.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-MacOSX
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-MacOSX
+CND_ARTIFACT_NAME_Release=_e_savitch9thed_chap1_pracprog5
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-MacOSX/_e_savitch9thed_chap1_pracprog5
+CND_PACKAGE_DIR_Release=dist/Release/GNU-MacOSX/package
+CND_PACKAGE_NAME_Release=(e)savitch9thedchap1pracprog5.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-MacOSX/package/(e)savitch9thedchap1pracprog5.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
