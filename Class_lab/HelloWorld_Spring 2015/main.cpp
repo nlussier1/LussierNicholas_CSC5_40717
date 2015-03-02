da1fb820b2f35af22f751fdec46c756ea9718b8a
nlussier1 <Nick_Lussier@nicholass-mbp.attlocal.net>
2015-03-02 06:41:06 -0800

    Hello World

---
+++ Class_lab/HelloWorld_Spring 2015/main.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.cpp
+ * Author: Nick_Lussier
+ * HelloWorld Spring 2015
+ * Created on February 18, 2015, 7:46 AM
+ */
+
+#include <iostream>
+
+using namespace std;
+//user libraries
+//global constants
+//function prototypes
+
+//execution beyond this point
+int main(int argc, char** argv) {
+    cout<<"Hello world!";
+    return 0;
+}
+
